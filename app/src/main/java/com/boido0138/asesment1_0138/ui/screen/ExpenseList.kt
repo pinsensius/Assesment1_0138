021e123585cc0a6ad4ad6c912783069936fbb9ed
pinsensius <vinsensiusboido@gmail.com>
2025-05-09 20:00:23 +0700

    Aplikasi sudah dapat mengganti tampilan dari list ke grid dan sebaliknya

--- app/src/main/java/com/boido0138/asesment1_0138/ui/screen/ExpenseList.kt
+++ app/src/main/java/com/boido0138/asesment1_0138/ui/screen/ExpenseList.kt
@@ -1,10 +1,12 @@
 package com.boido0138.asesment1_0138.ui.screen
 
         import android.content.res.Configuration
+        import androidx.compose.foundation.BorderStroke
         import androidx.compose.foundation.Image
         import androidx.compose.foundation.clickable
         import androidx.compose.foundation.layout.Arrangement
         import androidx.compose.foundation.layout.Column
+        import androidx.compose.foundation.layout.PaddingValues
         import androidx.compose.foundation.layout.Row
         import androidx.compose.foundation.layout.fillMaxSize
         import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,10 +14,14 @@ package com.boido0138.asesment1_0138.ui.screen
         import androidx.compose.foundation.layout.size
         import androidx.compose.foundation.lazy.LazyColumn
         import androidx.compose.foundation.lazy.items
+        import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+        import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+        import androidx.compose.foundation.lazy.staggeredgrid.items
         import androidx.compose.material.icons.Icons
         import androidx.compose.material.icons.automirrored.filled.ArrowBack
         import androidx.compose.material3.Card
         import androidx.compose.material3.CardDefaults
+        import androidx.compose.material3.DividerDefaults
         import androidx.compose.material3.ExperimentalMaterial3Api
         import androidx.compose.material3.Icon
         import androidx.compose.material3.IconButton
@@ -27,6 +33,9 @@ package com.boido0138.asesment1_0138.ui.screen
         import androidx.compose.runtime.Composable
         import androidx.compose.runtime.collectAsState
         import androidx.compose.runtime.getValue
+        import androidx.compose.runtime.mutableStateOf
+        import androidx.compose.runtime.remember
+        import androidx.compose.runtime.setValue
         import androidx.compose.ui.Alignment
         import androidx.compose.ui.Modifier
         import androidx.compose.ui.layout.ContentScale
@@ -50,6 +59,8 @@ package com.boido0138.asesment1_0138.ui.screen
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExpenseListScreen(navController: NavHostController) {
+    var showList by remember { mutableStateOf(true) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -68,11 +79,31 @@ fun ExpenseListScreen(navController: NavHostController) {
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
-                )
+                ),
+                actions = {
+                    IconButton(
+                        onClick = {
+                            showList = !showList
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(
+                                if(showList) R.drawable.baseline_grid_view_24
+                                else R.drawable.baseline_view_list_24
+                            ),
+                            contentDescription = stringResource(
+                                if(showList) R.string.grid_view
+                                else R.string.list_view
+                            ),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
             )
         }
     ) { innerPadding ->
         ExpenseListScreenContent(
+            showList,
             modifier = Modifier
                 .padding(innerPadding)
                 .padding(16.dp)
@@ -83,7 +114,7 @@ fun ExpenseListScreen(navController: NavHostController) {
 }
 
 @Composable
-fun ExpenseListScreenContent(modifier: Modifier = Modifier, navController: NavHostController) {
+fun ExpenseListScreenContent(showList : Boolean,modifier: Modifier = Modifier, navController: NavHostController) {
     val context = LocalContext.current
     val factory = ViewModelFactory(context)
     val viewModel : ExpenseViewModel = viewModel(factory = factory)
@@ -107,18 +138,84 @@ fun ExpenseListScreenContent(modifier: Modifier = Modifier, navController: NavHo
             )
         }
     } else {
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier,
-        ) {
-            items(data) {
-                DataCard(
-                    expense = it
-                ){
-                    navController.navigate(Screen.EditExpense.withId(it.id))
+        if(showList) {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier,
+            ) {
+                items(data) {
+                    DataCard(
+                        expense = it
+                    ) {
+                        navController.navigate(Screen.EditExpense.withId(it.id))
+                    }
                 }
             }
+        }else{
+            LazyVerticalStaggeredGrid(
+                modifier = modifier.fillMaxWidth(),
+                columns = StaggeredGridCells.Fixed(2),
+                verticalItemSpacing = 16.dp,
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                contentPadding = PaddingValues(bottom =  84.dp)
+            )  {
+                items(data){
+                    GridItem(
+                        expense = it
+                    ) {
+                        navController.navigate(Screen.EditExpense.withId(it.id))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun GridItem(expense: Expense, onClick: () -> Unit){
+    Card(
+        modifier = Modifier.fillMaxWidth().clickable {
+            onClick()
+        },
+        border = BorderStroke(0.9.dp, DividerDefaults.color),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ){
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = expense.title,
+                style = MaterialTheme.typography.titleMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            Text(
+                text = formatValues(expense.values),
+                style = MaterialTheme.typography.bodyLarge,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            Text(
+                text = stringResource(id = R.string.tags_data, expense.tags),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.bodySmall
+            )
+
+            Text(
+                text = stringResource(id = R.string.date_data, expense.date),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.bodySmall
+            )
         }
     }
 }
@@ -132,7 +229,8 @@ fun DataCard(expense: Expense, onClick : () -> Unit) {
                 onClick()
             },
         shape = MaterialTheme.shapes.medium,
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        border = BorderStroke(0.9.dp, DividerDefaults.color)
     ) {
         Column(
             verticalArrangement = Arrangement.spacedBy(8.dp),
--- app/src/main/java/com/boido0138/asesment1_0138/ui/screen/IncomeList.kt
+++ app/src/main/java/com/boido0138/asesment1_0138/ui/screen/IncomeList.kt
@@ -1,20 +1,26 @@
 package com.boido0138.asesment1_0138.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -26,6 +32,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -51,6 +60,8 @@ import java.util.Locale
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun IncomeListScreen(navController: NavHostController) {
+    var showList by remember { mutableStateOf(true) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -69,14 +80,34 @@ fun IncomeListScreen(navController: NavHostController) {
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
-                )
+                ),
+                actions = {
+                    IconButton(
+                        onClick = {
+                            showList = !showList
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(
+                                if(showList) R.drawable.baseline_grid_view_24
+                                else R.drawable.baseline_view_list_24
+                            ),
+                            contentDescription = stringResource(
+                                if(showList) R.string.grid_view
+                                else R.string.list_view
+                            ),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
             )
         }
     ) { innerPadding ->
         IncomeListScreenContent(
+            showList,
             modifier = Modifier
                 .padding(innerPadding)
-                .padding(16.dp)
+                .padding(horizontal = 16.dp, vertical = 16.dp)
                 .fillMaxSize()
             , navController = navController
         )
@@ -84,7 +115,7 @@ fun IncomeListScreen(navController: NavHostController) {
 }
 
 @Composable
-fun IncomeListScreenContent(modifier: Modifier = Modifier, navController: NavHostController) {
+fun IncomeListScreenContent(showList : Boolean,modifier: Modifier = Modifier, navController: NavHostController) {
     val context = LocalContext.current
     val factory = ViewModelFactory(context)
     val viewModel : IncomeViewModel = viewModel(factory = factory)
@@ -110,18 +141,76 @@ fun IncomeListScreenContent(modifier: Modifier = Modifier, navController: NavHos
             )
         }
     } else {
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier
-        ) {
-            items(data) {
-                DataCard(
-                    income = it
-                ){
-                    navController.navigate(Screen.EditIncome.withId(it.id))
+        if(showList) {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier
+            ) {
+                items(data) {
+                    DataCard(
+                        income = it
+                    ) {
+                        navController.navigate(Screen.EditIncome.withId(it.id))
+                    }
                 }
             }
+        }else{
+            LazyVerticalStaggeredGrid(
+                modifier = modifier.fillMaxWidth(),
+                columns = StaggeredGridCells.Fixed(2),
+                verticalItemSpacing = 8.dp,
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                contentPadding = PaddingValues(bottom = 84.dp)
+            )  {
+                items(data){
+                    GridItem(
+                        income = it
+                    ) {
+                        navController.navigate(Screen.EditIncome.withId(it.id))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun GridItem(income: Income, onClick: () -> Unit){
+    Card(
+        modifier = Modifier.fillMaxWidth().clickable {
+            onClick()
+        },
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        border = BorderStroke(0.9.dp, DividerDefaults.color)
+    ){
+        Column(
+            modifier = Modifier.padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = income.title,
+                style = MaterialTheme.typography.titleMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Text(
+                text = formatValues(income.values),
+                style = MaterialTheme.typography.bodyLarge,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+
+            Text(
+                text = stringResource(id = R.string.date_data, income.date),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.bodySmall
+            )
         }
     }
 }
@@ -135,7 +224,9 @@ fun DataCard(income: Income, onClick: () -> Unit) {
                 onClick()
             },
         shape = MaterialTheme.shapes.medium,
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        border = BorderStroke(0.9.dp, DividerDefaults.color)
+
     ) {
         Column(
             verticalArrangement = Arrangement.spacedBy(8.dp),
